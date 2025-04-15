72c9a4b39e791231124ee7792ee8681d0b232385
XZSt4nce <xzstnc@mail.ru>
2025-04-15 23:22:34 +0300

    1.1.2
    Signed-off-by: XZSt4nce <xzstnc@mail.ru>

--- include/reposaveeditor.h
+++ include/reposaveeditor.h
@@ -78,5 +78,5 @@ private:
 	void resizeEvent( QResizeEvent* event ) override;
 
 	inline static const QString Password = "Why would you want to cheat?... :o It's no fun. :') :'D";
-	inline static const QString Version = "1.1.1";
+	inline static const QString Version = "1.1.2";
 };
